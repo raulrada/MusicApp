b72579d5c6a66ee5525d5e5acf1f256caedb1422
raulrada <raul.rada@gmail.com>
2018-03-11 22:21:51 +0200

    Implemented song details in DetailsActivity and in related layout file

--- app/src/main/java/udacityscholarship/rada/raul/musicapp/DetailsActivity.java
+++ app/src/main/java/udacityscholarship/rada/raul/musicapp/DetailsActivity.java
@@ -1,5 +1,7 @@
 package udacityscholarship.rada.raul.musicapp;
 
+//todo store position onSaveInstanceState. it's the only one I really need
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -9,6 +11,11 @@ import java.util.ArrayList;
 public class DetailsActivity extends AppCompatActivity {
 
     private TextView songPositionTextView;
+    private TextView artistTextView;
+    private TextView titleTextView;
+    private TextView genreTextView;
+    private TextView yearTextView;
+
     private int position;
     private Song currentSong;
 
@@ -17,9 +24,6 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         setup();
-
-//        ArrayList<Song> t = new ArrayList<>();
-//        t = PlaylistActivity.songs;
     }
 
     /**
@@ -38,13 +42,22 @@ public class DetailsActivity extends AppCompatActivity {
         position = getIntent().getIntExtra("SONG_POSITION", 0);
         currentSong = PlaylistActivity.songs.get(position);
         songPositionTextView = (TextView) findViewById(R.id.song_position_text_view_details);
+        artistTextView = (TextView) findViewById(R.id.artist_text_view_details);
+        titleTextView = (TextView) findViewById(R.id.title_text_view_details);
+        genreTextView = (TextView) findViewById(R.id.genre_text_view_details);
+        yearTextView = (TextView) findViewById(R.id.year_text_view_details);
     }
 
     /**
      * populates activity layout
      */
     private void populateLayout(Song cSong) {
-        songPositionTextView.setText(String.valueOf(PlaylistActivity.songs.indexOf(cSong)+1));
+        int index = PlaylistActivity.songs.indexOf(cSong);
+        songPositionTextView.setText(String.valueOf(index+1));
+        artistTextView.setText(cSong.getSongArtist());
+        titleTextView.setText(cSong.getSongTitle());
+        genreTextView.setText(cSong.getSongGenre());
+        yearTextView.setText(String.valueOf(cSong.getSongYear()));
     }
 
 }
